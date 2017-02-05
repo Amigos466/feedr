09d3f903814a902836e46e97b90f4e13522a3fc9
Amigos466 <belousvladislav@gmail.com>
2017-02-05 11:52:31 +0200

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config';
 import request from 'request';
-import Feed from 'rss-to-json';
 import FeedParser from 'feedparser';
 
 var compiler = webpack(config)
